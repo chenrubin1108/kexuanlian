b3e515fc27798beaa3d558a2b5fba078e6f60b3e
陈如斌 <154724313@qq.com>
2021-04-14 15:33:55 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-
+require("@babel/core").transformSync("code", {
+  plugins: ["@babel/plugin-proposal-optional-chaining"],
+});
 new Vue({
   render: h => h(App),
 }).$mount('#app')
